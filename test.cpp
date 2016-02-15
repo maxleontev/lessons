353598c6a84a4357b3d958d3b9e6455837c78cb8
Max Leontev <_leon_@inbox.ru>
2016-02-15 12:08:16 +0800

    Lesson UNIX IPC shared memory. First task solving

--- test.cpp
+++ test.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-#include <unistd.h>
 #include <stdio.h>
-#include <errno.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
+
 //-------------------------------------------------------------------
 #define SH_MEM_SIZE 1024 * 1024 * 2
 //-------------------------------------------------------------------
-int main(int args, char ** argv) {
-
+int main() {
     key_t key;
     if ( (key = ftok("/tmp/mem.temp", 1)) == (key_t) -1) {
         perror("SysV error: ftok :");
@@ -19,10 +17,11 @@ int main(int args, char ** argv) {
     printf("key : %d\r\n", key);
 
     int shm_id;
-    if( (shm_id = shmget(key, SH_MEM_SIZE, IPC_CREAT | 0666)) == -1) {
+    if( (shm_id = shmget(key, SH_MEM_SIZE, 0666)) == -1) {
         perror("SysV error: shmget :");
         return 0;
     }
+
     printf("shm_id : %d\r\n", shm_id);
 
     char * shm;
@@ -31,18 +30,7 @@ int main(int args, char ** argv) {
           return 0;
     }
 
-    for (int i = 0; i < 1024 * 1024; i++)
-            *(shm + i) = i%127;
-
-    printf("press 1 to detach shared memory and exit\r\n");
-    int d = 0;
-    std::cin >> d;
-    if(d==1) {
-        if( shmdt(shm) == -1) {
-            perror("SysV error: shmdt :");
-            return 0;
-        }
-    }
+    printf("%d %d %d %d \r\n", *(shm+0), *(shm+1), *(shm+2), *(shm+3) );
 
     return 0;
 }

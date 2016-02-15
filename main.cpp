174b71ea5e7d8b180042ab5adb1b876cf89838ff
Max Leontev <_leon_@inbox.ru>
2016-02-15 12:04:18 +0800

    Lesson UNIX IPC queue. Second task solving

--- main.cpp
+++ main.cpp
@@ -1,49 +1,44 @@
-#include <iostream>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <mqueue.h>
 
 //-------------------------------------------------------------------
 
-#define MSG_F_NAME "/tmp/msg.temp"
-#define MSG_FILE_NAME "/home/box/message.txt"
-//#define MSG_FILE_NAME "/media/sf_mint/projects/qt/lessons/01/mess.txt"
+#define POSIX_MSG_F_NAME "/test.mq"
+//#define MSG_FILE_NAME "/home/box/message.txt"
+#define MSG_FILE_NAME "/media/sf_mint/projects/qt/lessons/01/mess.txt"
+
+#define MAX_SIZE 1024
 
 using namespace std;
 //-------------------------------------------------------------------
 int main(int argc, char *argv[]) {
+    mqd_t mq_key;
 
-    key_t key;
-    if ( (key = ftok(MSG_F_NAME, 1)) == (key_t) -1) {
-        perror("IPC error: ftok");
+    struct mq_attr attr;
+    attr.mq_flags = O_NONBLOCK;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = MAX_SIZE;
+    attr.mq_curmsgs = 0;
+
+    if( (mq_key = mq_open(POSIX_MSG_F_NAME, O_RDONLY|O_NONBLOCK|O_CREAT, 0660, &attr) ) == (mqd_t) -1 ) {
+        perror("POSIX error: mq_open :");
         return 0;
     }
-
-    int msg_id;
-    if( (msg_id = msgget(key, IPC_CREAT | 0777)) == -1) {
-        perror("IPC error: msgget");
-        return 0;
+    char buff[MAX_SIZE];
+    ssize_t res;
+    while(1) {
+        res = mq_receive(mq_key, buff, MAX_SIZE, NULL);
+        if(res != -1) {
+            int f = open (MSG_FILE_NAME, O_RDWR|O_CREAT, 0660);
+            if( write(f, buff, res) == -1){
+                perror("write error:");
+                return 0;
+            }
+            close(f);
+        }
     }
-
-    struct message {
-            long mtype;
-            char mtext[80];
-    } mess;
-
-    int res = -1;
-    while(res == -1) {
-        res = msgrcv(msg_id, &mess, sizeof(message) - sizeof(long), 0, MSG_NOERROR|IPC_NOWAIT);
-    }
-    std::cout << "res =" << res << std::endl;
-
-    int f = open (MSG_FILE_NAME, O_RDWR|O_CREAT, 0660);
-    if( write(f, mess.mtext, res) == -1){
-        perror("write error:");
-        return 0;
-    }
-    close(f);
 
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,19 +1,7 @@
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
-#include <string.h>
-
-struct message {
-    long mtype;
-    char mtext[80];
-};
+#include <mqueue.h>
 
 int main(){
-    key_t key = ftok("/tmp/msg.temp", 1);
-    int id = msgget(key, 0666);
-    struct message m;
-    m.mtype = 1;
-    strcpy(m.mtext, "Hello, world!");
-    msgsnd(id, (struct msgbuf *) &m, 13, 0);
+    int mqd = mq_open("/test.mq", O_RDWR);
+    mq_send(mqd, "Hello, world, good boy!", 23, 0);
     return 0;
 }

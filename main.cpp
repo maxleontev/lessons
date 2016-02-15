d80ec557e00676b3814d62b16ecaf4b99f0a76a1
Max Leontev <_leon_@inbox.ru>
2016-02-15 11:54:17 +0800

    Lesson Unix channels. Third task solving.

--- main.cpp
+++ main.cpp
@@ -1,65 +1,28 @@
-/*
-#include <sstream>
-#include <vector>
-#include <fcntl.h>
-#include <fstream>
-#include <unistd.h>
-*/
-
 #include <iostream>
-
 #include <sys/types.h>
-#include <sys/stat.h>
-
-#include <stdio.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
-#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 //-------------------------------------------------------------------
-
-/*
-#define IN_F_N "/home/box/in.fifo"
-#define OUT_F_N "/home/box/out.fifo"*/
-
-#define IN_F_N "/tmp/in.fifo"
-#define OUT_F_N "/tmp/out.fifo"
-
-/*#define IN_F_N "/media/sf_mint/projects/qt/lessons/01/in.fifo"
-#define OUT_F_N "/media/sf_mint/projects/qt/lessons/01/out.fifo"*/
-
-
 using namespace std;
 //-------------------------------------------------------------------
 int main(int argc, char *argv[]) {
 
-    unlink(IN_F_N);
-    unlink(OUT_F_N);
-
-    if( (mkfifo(IN_F_N, 0660)) == -1) {
-        cout << "error creating in_fifo" << endl;
-        return 0;
-    }
-    int in_fifo;
-    if( (in_fifo = open(IN_F_N, O_RDWR)) == -1 ) {
-        cout << "error open in_fifo" << endl;
+    int sockets[2];
+    if (socketpair(AF_UNIX, SOCK_STREAM, 0, sockets) < 0) {
+        cout << "socketpair() failed" << endl;
         return 0;
     }
 
-    if( (mkfifo(OUT_F_N, 0660)) == -1) {
-        cout << "error creating out_fifo" << endl;
-        return 0;
-    }
-    int out_fifo;
-    if( (out_fifo = open(OUT_F_N, O_RDWR)) == -1 ) {
-        cout << "error open out_fifo" << endl;
-        return 0;
-    }
+    if (fork() != 0)
+        close(sockets[1]);
+    else
+        close(sockets[0]);
 
-    int ln;
-    char buf[1];
-    while ((ln = read(in_fifo, buf, 1)) != 0)
-        write(out_fifo, buf, ln);
+    while(1);
 
     return 0;
 }

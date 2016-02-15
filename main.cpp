62aa04abd8366c3efda106b480a88a2d9e22c5f8
Max Leontev <_leon_@inbox.ru>
2016-02-15 11:10:18 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <cstdio>
+#include "signal.h"
+
+
+//-------------------------------------------------------------------
+int main(int argc, char *argv[])
+{
+    FILE * p_file = fopen ("pid","w");
+    fprintf(p_file, "%d", getpid());
+    fclose(p_file);
+
+    signal(SIGTERM, SIG_IGN);
+    signal(SIGINT, SIG_IGN);
+
+    while(1);
+
+    return 0;
+}
+//-------------------------------------------------------------------

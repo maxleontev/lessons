d735ce137f24a06d18631ac8a70d7de87b0e54e2
Max Leontev <_leon_@inbox.ru>
2016-02-17 11:55:11 +0800

    OpenMP worked example three.

--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@ int main(int args, char ** argv) {
 #pragma omp parallel num_threads(4)
 {
     std::cout << 1 << std::endl;
-    std::cout << 2 << std::endl;
-    #pragma omp barrier
+    #pragma omp single nowait
+    {
+        std::cout << 2 << std::endl;
+    }
     std::cout << 3 << std::endl;
 }
-
     return 0;
 }
 //-------------------------------------------------------------------

770b3ae253b3d7d473c066871f8412ce9cefffc5
Max Leontev <_leon_@inbox.ru>
2016-02-18 13:41:48 +0800

    Intel TBB. Worked example two

--- main.cpp
+++ main.cpp
@@ -15,13 +15,13 @@
 using namespace std;
 //-------------------------------------------------------------------
 
-void Foo(int k) {
-    std::cout << k << std::endl;
+void Foo(int k, int p_beg) {
+    std::cout << p_beg << " :---: " << k << std::endl;
 }
 //-------------------------------------------------------------------
 void SerialApplyFoo(int arr[], size_t size) {
     for(size_t i=0; i<size; i++)
-        Foo(arr[i]);
+        Foo(arr[i], 0);
 }
 //-------------------------------------------------------------------
 class ApplyFoo {
@@ -30,26 +30,20 @@ public:
     void operator()(const tbb::blocked_range<size_t> & r) const {
         int * a = my_a;
         for(size_t i=r.begin(); i!=r.end(); ++i)
-            Foo(a[i]);
+            Foo(a[i], r.begin());
     }
     ApplyFoo(int a[]) : my_a(a) {}
 };
 //-------------------------------------------------------------------
 void ParallelApplyFoo(int arr[], size_t size) {
-    tbb::parallel_for(tbb::blocked_range<size_t>(0, size, 50), ApplyFoo(arr) );
+    tbb::parallel_for(tbb::blocked_range<size_t>(0, size, 10),
+                      ApplyFoo(arr),
+                      tbb::simple_partitioner() );
 }
-//-------------------------------------------------------------------
-/*void ParallelApplyFoo(int arr[], size_t size) {
-    tbb::parallel_for(tbb::blocked_range<size_t>(0, size),
-    [=] (const tbb::blocked_range<size_t>&r) {
-        for(size_t i=r.begin(); i!=r.end(); i++)
-            Foo(arr[i]);
-    } );
-}*/
 //-------------------------------------------------------------------
 int main(int argc, char ** argv) {
 
-    if(argc < 2) {
+/*    if(argc < 2) {
         cout << "argument is absent" << endl;
         return 0;
     }
@@ -65,20 +59,21 @@ int main(int argc, char ** argv) {
 
     ifstream if_str(argv[1]);
 
-    int k;
+    int k;*/
     vector <int> data;
-    while(if_str >> k)
-        data.push_back(k);
+//    while(if_str >> k)
+    for(int i=0;i<500;i++)
+        data.push_back(i);
 
     ParallelApplyFoo(data.data(), data.size());
 //    SerialApplyFoo(data.data(), data.size());
 
-    ofstream of_str("out.txt");
+/*    ofstream of_str("out.txt");
     for(auto it : data) {
         of_str << it;
         if(it != data.back())
             of_str << " ";
-    }
+    }*/
 
     return 0;
 }

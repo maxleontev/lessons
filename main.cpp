159f85c40cb132ae779b2d88890de9a4a809f4c7
Max Leontev <_leon_@inbox.ru>
2016-02-18 15:29:19 +0800

    Intel TBB. Worked example four.

--- main.cpp
+++ main.cpp
@@ -1,45 +1,35 @@
-#include <vector>
 #include <iostream>
+#include <list>
 
-#include <tbb/blocked_range.h>
-#include <tbb/parallel_reduce.h>
+#include <tbb/parallel_do.h>
 
 //-------------------------------------------------------------------
-class SumFoo {
-    const int * my_arr;
+void Foo(int k) {
+    std::cout << k << std::endl;
+}
+//-------------------------------------------------------------------
+void SerialApplyFooToList(const std::list <int> & list) {
+    for( std::list<int>::const_iterator it = list.begin(); it != list.end() ; it++  ){
+        Foo(*it);
+    }
+}
+//-------------------------------------------------------------------
+class ApplyFoo {
 public:
-    int my_sum;
-    void operator()(const tbb::blocked_range<size_t> & r)  {
-        int * a = (int *) my_arr;
-        int sum = my_sum;
-        size_t end = r.end();
-        for(size_t i=r.begin(); i!=r.end(); ++i)
-            sum += a[i];
-        my_sum = sum;
-    }
-
-    SumFoo(SumFoo &x, tbb::split) : my_arr(x.my_arr), my_sum(0) {
-    }
-
-    void join(const SumFoo &y) {
-        my_sum += y.my_sum;
-    }
-    SumFoo(const int a[]) : my_arr(a), my_sum(0) {}
+    void operator()(const int & item) const { Foo(item); }
 };
 //-------------------------------------------------------------------
-int ParallelSumFoo(const int arr[], size_t size) {
-    SumFoo sf(arr);
-    tbb::parallel_reduce(tbb::blocked_range<size_t>(0, size), sf);
-    return sf.my_sum;
+void ParallelApplyFooToList(const std::list <int> & list) {
+    tbb::parallel_do(list.begin(), list.end(), ApplyFoo());
 }
 //-------------------------------------------------------------------
 int main() {
-    std::vector <int> data;
+    std::list <int> list;
     for(int i=0;i<500;i++)
-        data.push_back(i);
+        list.push_back(i);
 
-    int res = ParallelSumFoo( data.data(), data.size() );
-    std::cout << res << std::endl;
+//    SerialApplyFooToList(list);
+    ParallelApplyFooToList(list);
 
     return 0;
 }

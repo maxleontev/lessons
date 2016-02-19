eeb9842b4475ff1838bb7160a63c28d495f17810
Max Leontev <_leon_@inbox.ru>
2016-02-19 12:55:39 +0800

    Parallel programming section final task solving

--- main.cpp
+++ main.cpp
@@ -2,50 +2,65 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include <algorithm>
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-#include <tbb/parallel_for.h>
-#include <tbb/blocked_range.h>
-
 using namespace std;
-//-------------------------------------------------------------------
 
-void Foo(int k) {
-    std::cout << k << std::endl;
-}
 //-------------------------------------------------------------------
-void SerialApplyFoo(int arr[], size_t size) {
-    for(size_t i=0; i<size; i++)
-        Foo(arr[i]);
-}
-//-------------------------------------------------------------------
-class ApplyFoo {
-    int * my_a;
-public:
-    void operator()(const tbb::blocked_range<size_t> & r) const {
-        int * a = my_a;
-        for(size_t i=r.begin(); i!=r.end(); ++i)
-            Foo(a[i]);
+void Merge(vector <int>& v, int a_bg, int a_ln, int b_bg, int b_ln, vector <int>& c) {
+    int i=a_bg;
+    int j=b_bg;
+    while( i < (a_bg+a_ln) && j < (b_bg+b_ln) ) {
+        if( v[i] <= v[j] ) {
+            c[(i-a_bg)+(j-b_bg)] = v[i];
+            i++;
+        } else {
+            c[(i-a_bg)+(j-b_bg)] = v[j];
+            j++;
+        }
     }
-    ApplyFoo(int a[]) : my_a(a) {}
-};
-//-------------------------------------------------------------------
-void ParallelApplyFoo(int arr[], size_t size) {
-    tbb::parallel_for(tbb::blocked_range<size_t>(0, size, 50), ApplyFoo(arr) );
+
+    if( i == (a_bg+a_ln)) {
+        for(; j<b_bg+b_ln; j++)
+            c[(i-a_bg)+(j-b_bg)] = v[j];
+    } else if( j == (b_bg+b_ln) ) {
+        for(; i<a_bg+a_ln; i++)
+            c[(i-a_bg)+(j-b_bg)] = v[i];
+    }
 }
 //-------------------------------------------------------------------
-/*void ParallelApplyFoo(int arr[], size_t size) {
-    tbb::parallel_for(tbb::blocked_range<size_t>(0, size),
-    [=] (const tbb::blocked_range<size_t>&r) {
-        for(size_t i=r.begin(); i!=r.end(); i++)
-            Foo(arr[i]);
-    } );
-}*/
+void MergeSort(std::vector <int> & v, int bg, int length) {
+    if(length <= 1)
+        return;
+    int fL = length / 2;
+    int sL = length - fL;
+
+#pragma omp parallel
+{
+    #pragma omp sections
+    {
+        #pragma omp section
+        {
+            MergeSort(v, bg, fL);
+        }
+        #pragma omp section
+        {
+            MergeSort(v, bg + fL, sL);
+        }
+    }
+}
+
+    std::vector <int> c(length);
+
+    Merge(v, bg, fL, bg+fL, sL, c);
+
+    for(int i=0; i<length; i++)
+        v[bg + i] = c[i];
+}
 //-------------------------------------------------------------------
 int main(int argc, char ** argv) {
 
@@ -70,8 +85,7 @@ int main(int argc, char ** argv) {
     while(if_str >> k)
         data.push_back(k);
 
-    ParallelApplyFoo(data.data(), data.size());
-//    SerialApplyFoo(data.data(), data.size());
+    MergeSort(data, 0, data.size());
 
     ofstream of_str("out.txt");
     for(auto it : data) {
@@ -79,6 +93,8 @@ int main(int argc, char ** argv) {
         if(it != data.back())
             of_str << " ";
     }
+    of_str << endl;
+    cout << "result has been saved" << endl;
 
     return 0;
 }

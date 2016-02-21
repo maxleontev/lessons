dbe65843f2f81fcc37043247847254c0f0a8d184
Max Leontev <_leon_@inbox.ru>
2016-02-21 15:08:09 +0800

    libev worked example in simple C code form

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	g++ main.cpp -std=c++11 -o m -lrt -pthread
+	g++ main.cpp -lev -std=c++11 -o m
--- main.cpp
+++ main.cpp
@@ -1,22 +1,55 @@
-#include <iostream>
-
-#include <sys/types.h>
-#include <unistd.h>
-#include <cstdio>
-#include "signal.h"
-
+#include <stdio.h>
+#include <netinet/in.h>
+#include <ev.h>
+#include <strings.h>
+#include <stdlib.h>
 
 //-------------------------------------------------------------------
-int main(int argc, char *argv[])
+void read_cb(struct ev_loop *loop, struct ev_io *watcher, int revents) {
+    char buffer[1024];
+    ssize_t r = recv(watcher->fd, buffer, 1024, MSG_NOSIGNAL);
+    if(r < 0) {
+        return;
+    } else if(r == 0) {
+        ev_io_stop(loop, watcher);
+        free(watcher);
+        return;
+    } else {
+        send(watcher->fd, buffer, r, MSG_NOSIGNAL);
+    }
+}
+//-------------------------------------------------------------------
+void accept_cb(struct ev_loop *loop, struct ev_io *watcher, int revents) {
+    int client_sd = accept(watcher->fd, 0, 0);
+
+    struct ev_io *w_client = (struct ev_io*) malloc(sizeof(struct ev_io));
+
+    ev_io_init(w_client, read_cb, client_sd, EV_READ);
+    ev_io_start(loop, w_client);
+}
+//-------------------------------------------------------------------
+int main(int argc, char **argv)
 {
-    FILE * p_file = fopen ("pid","w");
-    fprintf(p_file, "%d", getpid());
-    fclose(p_file);
+    struct ev_loop *loop = ev_default_loop(0);
 
-    signal(SIGTERM, SIG_IGN);
-    signal(SIGINT, SIG_IGN);
+    int sd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    struct sockaddr_in addr;
+    bzero(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(12345);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    bind(sd, (struct sockaddr*)&addr, sizeof(addr));
 
-    while(1);
+    listen(sd, SOMAXCONN);
+
+    struct ev_io w_accept;
+    ev_io_init(&w_accept, accept_cb, sd, EV_READ);
+
+    ev_io_start(loop, &w_accept);
+
+    while(1) {
+        ev_loop(loop, 0);
+    }
 
     return 0;
 }
